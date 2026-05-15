104337a42d81d8d5c6923d2634fe1ab51f616253
Luis Cortez <danielcorteznew8@gmail.com>
2026-05-15 00:32:20 -0400

    system navbar

--- src/app/rptos/seccion-sistema/seccion-sistema.component.ts
+++ src/app/rptos/seccion-sistema/seccion-sistema.component.ts
@@ -1,42 +1,10 @@
 import { Component } from '@angular/core';
 
-interface SideNavToggle{
-  screenWidth: number;
-  collapsed: boolean;
-}
-
-
 @Component({
   selector: 'app-seccion-productos',
   templateUrl: './seccion-sistema.component.html',
   styleUrls: ['./seccion-sistema.component.css']
 })
 export class SeccionSistemaComponent {
-
-  isSideNavCollapsed = false;
-  screenWidth = 0;
-
-  onToggleSideNav(data: SideNavToggle): void{
-    this.screenWidth = data.screenWidth;
-    this.isSideNavCollapsed = data.collapsed;
-  }
-
-  // public sidebarItems = [
-  //   {label: 'Listado', icon: 'label', url: './list'},
-  //   {label: 'Añadir', icon: 'add', url: './new-hero'},
-  //   {label: 'Buscar', icon: 'search', url: './search'},
-  // ]
-
-  public navbarData = [
-    {
-      url:'lista-producto',
-      icon: 'list_alt',
-      label: 'Llanito System'
-    },
-    // {
-    //   url:'lista-producto-en-web',
-    //   icon: 'travel_explore',
-    //   label: 'Productos en la Web'
-    // },
-  ]
+  // Componente simplificado - solo muestra el contenido del router-outlet
 }
